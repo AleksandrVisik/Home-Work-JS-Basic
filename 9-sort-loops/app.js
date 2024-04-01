80367b4f301eb908e51fada4480f0a5607decacd
Aleksandr Visotskiy <sasavisik@gmail.com>
2024-04-01 08:20:58 +0300

    9-sort-loops-del-error

--- 9-sort-loops/app.js
+++ 9-sort-loops/app.js
@@ -1,9 +1,10 @@
-const arr = [1, 40, -5, 10, 0];
+const arr = [1, 40, -5, 10, 0,];
+const arr1 = [15, 21, 0, -29, -2, 16];
 function sortArrUp(array) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] > arr[j]) {
-                [arr[i], arr[j]] = [arr[j], arr[i]]
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] > array[j]) {
+                [array[i], array[j]] = [array[j], array[i]]
             }
         }
     }
@@ -13,5 +14,5 @@ function sortArrDown(array) {
     const down = sortArrUp(array);
     return down.reverse();
 }
-console.log(`В порядке возрастания: ${sortArrUp(arr).join(", ")}`);
+console.log(`В порядке возрастания: ${sortArrUp(arr1).join(", ")}`);
 console.log(`В порядке убывания: ${sortArrDown(arr).join(", ")}`);
